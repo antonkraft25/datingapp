be7f6e5fdf0b2315954a7cd8ca784696888f8c4a
antonkraft25 <antonkraft25@gmail.com>
2026-03-08 23:43:01 +0100

    feat(API): add error handling and testing components; implement exception middleware, error DTOs, and test error routes

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,10 +7,13 @@ public class RegisterDto
 {
     [Required]
     public string DisplayName { get; set; } = "";
+
     [Required]
     [EmailAddress]
     public string Email { get; set; } ="";
+
     [Required]
     [MinLength(4)]
     public string Password { get; set; } = "";
+    
 }
