c7e3ab709ae6936f178aebc0997eb1420cefaf19
antonkraft25 <antonkraft25@gmail.com>
2026-03-16 16:57:54 +0100

    feat(client, API): add photo management features; implement set main photo and delete photo functionality; enhance member photos component with star and delete buttons

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -2,18 +2,22 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, single } from 'rxjs';
-import { Photo } from '../../../types/member';
-import { AsyncPipe } from '@angular/common';
+import { Member, Photo } from '../../../types/member';
 import { ImageUpload } from "../../../shared/image-upload/image-upload";
+import { AccountService } from '../../../core/services/account-service';
+import { User } from '../../../types/user';
+import { StarButton } from "../../../shared/star-button/star-button";
+import { DeleteButton } from "../../../shared/delete-button/delete-button";
 
 @Component({
   selector: 'app-member-photos',
-  imports: [ImageUpload],
+  imports: [ImageUpload, StarButton, DeleteButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css',
 })
 export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService); 
+  protected accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
   protected photos = signal<Photo[]>([]);
   protected loading = signal(false);
@@ -40,6 +44,28 @@ export class MemberPhotos implements OnInit {
         this.loading.set(false);
       }
       
+    })
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.memberService.setMainPhoto(photo).subscribe({
+      next: () => {
+        const currentUser = this.accountService.currentUser();
+        if (currentUser) currentUser.imageUrl = photo.url; 
+        this.accountService.setCurrentUser(currentUser as User);
+        this.memberService.member.update(member => ({
+          ...member,
+          imageUrl: photo.url
+        }) as Member)
+      }
+    })
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: () => {
+        this.photos.update(photos => photos.filter(x => x.id !== photoId))
+      }
     })
   }
 }
